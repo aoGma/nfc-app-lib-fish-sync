5583a82ef2b91ab3b521832b9eb66e9ec095bf3e
Leo Yao <159001000+aoGma@users.noreply.github.com>
2025-07-07 10:56:44 +0800

    Update _find_latest_nfc_folder.fish

--- functions/_find_latest_nfc_folder.fish
+++ functions/_find_latest_nfc_folder.fish
@@ -1,5 +1,9 @@
 function _find_latest_nfc_folder
     set -l download_dir (set -q NFC_DOWNLOAD_DIR; and echo $NFC_DOWNLOAD_DIR; or echo "$HOME/Downloads")
-    set -l folders (ls -dt "$download_dir"/nfc-app-lib*/ 2>/dev/null)
-    echo (string split \n -- $folders)[1]
+    set -l matched_dirs (for d in $download_dir/nfc-app-lib*/; test -d $d; and echo $d; end)
+    if test (count $matched_dirs) -eq 0
+        return
+    end
+    set -l sorted (printf "%s\n" $matched_dirs | sort -r)
+    echo $sorted[1]
 end
